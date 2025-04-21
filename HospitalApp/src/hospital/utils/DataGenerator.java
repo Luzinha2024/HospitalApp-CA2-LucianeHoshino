6f66d79bc2a66cdb6c13fa77e645f20fc50bc7df
Luciane Hoshino <2024507@student.cct.ie>
2025-04-21 18:43:05 +0100

    Start the DataGenerator class

--- HospitalApp/src/hospital/utils/DataGenerator.java
+++ HospitalApp/src/hospital/utils/DataGenerator.java
@@ -5,10 +5,24 @@
 
 package hospital.utils;
 
+import hospital.enums.Department;
+import hospital.enums.ManagerType;
+import hospital.model.Person;
+import java.util.Random;
+
 /**
  *
- * @author Luciane Hoshino
+ * @author lucia
  */
 public class DataGenerator {
-
+    
+     private static final String[] names = {"Acerola Souza", "Bruno Lucas", "Carlos Alberto", "Diana Silva", "Eva Martins", "Fábio Rocha"};
+    
+   public static Person generateRandomPerson() {
+        Random rand = new Random();
+        String name = names[rand.nextInt(names.length)];
+        ManagerType manager = ManagerType.values()[rand.nextInt(ManagerType.values().length)];
+        Department dept = Department.values()[rand.nextInt(Department.values().length)];
+        return new Person(name, manager, dept);
+    }
 }
