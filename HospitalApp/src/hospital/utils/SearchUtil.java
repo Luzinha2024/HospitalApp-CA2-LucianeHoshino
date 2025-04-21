9b60b06c5925b3d241a26c42881c82dc0b657eb2
Luciane Hoshino <2024507@student.cct.ie>
2025-04-21 20:56:26 +0100

    Start the SearchUtil class

--- HospitalApp/src/hospital/utils/SearchUtil.java
+++ HospitalApp/src/hospital/utils/SearchUtil.java
@@ -5,10 +5,28 @@
 
 package hospital.utils;
 
+import hospital.model.Person;
+import java.util.List;
+
 /**
  *
- * @author Luciane Hoshino
+ * @author lucia
  */
 public class SearchUtil {
 
+   public static Person binarySearch(List<Person> list, String target) {
+        int left = 0, right = list.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            Person midPerson = list.get(mid);
+
+            int result = target.compareToIgnoreCase(midPerson.getName());
+
+            if (result == 0) return midPerson;
+            if (result < 0) right = mid - 1;
+            else left = mid + 1;
+        }
+        return null;
+    }
 }
