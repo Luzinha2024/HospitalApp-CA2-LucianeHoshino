63057083c5a73b11aba2b65bd6f8829a47fd5ecc
Luciane Hoshino <2024507@student.cct.ie>
2025-04-26 18:04:54 +0100

    I using Insertion Sort, I am changing my code. Because Insertion in this case is more stable and for smalls data base.

--- HospitalApp/src/hospital/utils/SortUtil.java
+++ HospitalApp/src/hospital/utils/SortUtil.java
@@ -13,29 +13,13 @@ import java.util.List;
  * @author lucia
  */
 public class SortUtil {
+    
+    
+    public static void insertionSort(List<Person> list) {
+    for (int i = 1; i < list.size(); i++) {
+        Person key = list.get(i);
+        int j = i - 1;
 
-public static void quickSort(List<Person> list, int low, int high) {
-    if (low < high) {
-        int pi = partition(list, low, high);
-        quickSort(list, low, pi - 1);
-        quickSort(list, pi + 1, high);
-    }
-}
+        
+    
 
-private static int partition(List<Person> list, int low, int high) {
-    String pivot = list.get(high).getName();
-    int i = low - 1;
-    for (int j = low; j < high; j++) {
-        if (list.get(j).getName().compareToIgnoreCase(pivot) <= 0) {
-            i++;
-            Person temp = list.get(i);
-            list.set(i, list.get(j));
-            list.set(j, temp);
-        }
-    }
-    Person temp = list.get(i + 1);
-    list.set(i + 1, list.get(high));
-    list.set(high, temp);
-    return i + 1;
-}
-  }
