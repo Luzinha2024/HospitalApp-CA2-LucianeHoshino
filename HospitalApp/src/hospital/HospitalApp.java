b3f987360a0b3b48b27318553a48d19efafc7223
Luciane Hoshino <2024507@student.cct.ie>
2025-04-27 21:40:33 +0100

    Now I am start the option Menu using Switch/Case

--- HospitalApp/src/hospital/HospitalApp.java
+++ HospitalApp/src/hospital/HospitalApp.java
@@ -4,6 +4,9 @@
  */
 package hospital;
 
+import hospital.enums.MenuOption;
+import static hospital.enums.MenuOption.EXIT;
+import static hospital.enums.MenuOption.SAVE;
 import hospital.model.Employee;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,11 +28,43 @@ import java.util.Scanner;
             System.out.println("\nPlease select an option:");
             for (hospital.enums.MenuOption option : hospital.enums.MenuOption.values()) {
                 System.out.println(option.ordinal() + 1 + ". " + option);
- 
             }
+              int choice = -1;
+         try {
+            System.out.print("Your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+         
+}       catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a number.");
+            continue; // Go back to start to while
+}
+        if (choice < 1 || choice > MenuOption.values().length) {
+          System.out.println("Invalid option. Please select between 1 and " + MenuOption.values().length + ".");
+         continue;
+}
+        if (choice < 1 || choice > MenuOption.values().length) {
+          System.out.println("Invalid option. Please select between 1 and " + MenuOption.values().length + ".");
+         continue;
+}
 
+            switch (hospital.enums.MenuOption.values()[choice - 1]) {
+                case SORT_EMPLOYEE -> sortFromFile();
+                case SEARCH_EMPLOYEE -> searchByName();
+                case ADD_EMPLOYEE -> addNewPerson();
+                case GENERATE_RANDOM_EMPLOYEE -> generateRandomEmployee();
+                case ALL_EMPLOYEES -> displayAllEmployees();              
+                case SAVE -> savePeopleToFile();              
+                case EXIT -> {
+                    System.out.println("Exiting...");
+                    return;
+                }
+            }
+        }
+
+        }
             
     }
-         }
-           }
+         
+           
         
