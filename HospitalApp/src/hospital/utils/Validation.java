058251e024e5cddefe2987e259509776fca38267
Luciane Hoshino <2024507@student.cct.ie>
2025-04-28 18:36:20 +0100

    Comment the code Validation class

--- HospitalApp/src/hospital/utils/Validation.java
+++ HospitalApp/src/hospital/utils/Validation.java
@@ -9,13 +9,26 @@ package hospital.utils;
  *
  * @author lucia
  */
-        public class Validation {
+       public class Validation {// Utility class that provides input validation methods.
 
-        public static boolean isValidName(String name) {
-          return name != null && name.matches("[A-Za-z ]+");
+          public static boolean isValidName(String name) {/* Method to check if a given name is valid.
+                                                           * It must not be null and must contain only letters 
+                                                           *(uppercase or lowercase) and spaces.*/
+            
+            return name != null && name.matches("[A-Za-z ]+"); /* Returns true if: // - the name is not null, and
+                                                            * - it matches the regular expression "[A-Za-z ]+",
+                                                            * which means the name can only include: // - uppercase letters (A-Z)
+                                                            * - lowercase letters (a-z)
+                                                            * - spaces
+                                                            * Any digits, special characters, or empty strings will cause
+                                                            *it to return false.*/
           
-    }
+         
+}
+       }
+          
+    
         
 
-}
+
           
