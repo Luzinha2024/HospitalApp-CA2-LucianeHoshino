66b685d3bcf82119cae1db9c4f4b040050f9922f
Luciane Hoshino <2024507@student.cct.ie>
2025-04-29 21:19:13 +0100

    I add label and toString(), to make more interactive and look better the Menu and also in ManagerType and Department

--- HospitalApp/src/hospital/enums/Department.java
+++ HospitalApp/src/hospital/enums/Department.java
@@ -9,10 +9,22 @@ package hospital.enums;
  *
  * @author lucia
  */
-public enum Department {
+    public enum Department {
   
-    EMERGENCY,
-    TECH_SUPPORT,
-    HR,
-    PHYSIOTHERAPY
+       // Each department type has a custom label for better display
+       EMERGENCY("Emergency Department"),        // Department handling urgent medical cases
+       TECH_SUPPORT("Technical Support"),         // Department responsible for technical assistance
+       HR("Human Resources"),                     // Department managing employee relations
+       PHYSIOTHERAPY("Physiotherapy Department"); // Department focused on physical rehabilitation
+    
+   private final String label; // This variable stores the friendly label for each manager type
+
+    Department(String label) { // Constructor: receives the label and stores it in the 'label' variable
+        this.label = label;// Assigns the label to each enum value
+    }
+
+    @Override // Overrides the default toString() method to return the friendly label
+public String toString() {
+        return label;// So when printed, it shows "Team Lead" instead of TEAM_LEAD
+    }
 }
