235ad821355eaa4f2eb889be24298e55a7386e6d
Luciane Hoshino <2024507@student.cct.ie>
2025-04-30 12:08:19 +0100

    Moved all classes to package 'hospital' and changed 'name' field in Person from private to protected for inheritance support in Employee.

---
+++ HospitalApp/src/hospital/Person.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package hospital;
+
+
+/**
+ *
+ * @author lucia
+ */
+    public class Person {
+    
+  
+    public String name;
+
+      public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    /*private String name;
+    private ManagerType managerType;
+    private Department department;
+
+    public Person(String name, ManagerType managerType, Department department) {
+        this.name = name;
+        this.managerType = managerType;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ManagerType getManagerType() {
+        return managerType;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + managerType + " - " + department;
+    }
+}*/
