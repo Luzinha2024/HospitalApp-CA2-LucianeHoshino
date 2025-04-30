db60e02bfb451233ca88b5959fd34218a8eb21d7
Luciane Hoshino <2024507@student.cct.ie>
2025-04-30 12:04:40 +0100

    I moved all classes to one Packege, to packege hospital

---
+++ HospitalApp/src/hospital/DataGenerator.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package hospital;
+import java.util.Random;
+
+/**
+ *
+ * @author lucia
+ */
+public class DataGenerator {
+    
+     private static final String[] names = {"Alice Souza", "Bruno De Lucas", "Carlos Almeida", "Diana Smith", "Eva Martins", "Fábio Martins", 
+         "Gabriel Pensador", "Laura Pausine", "Marcelo Telo"};
+    
+   public static Employee generateRandomPerson() {
+        Random rand = new Random();
+        String name = names[rand.nextInt(names.length)];
+        ManagerType manager = ManagerType.values()[rand.nextInt(ManagerType.values().length)];
+        Department dept = Department.values()[rand.nextInt(Department.values().length)];
+        return new Employee(name, manager, dept);
+    }
+}
