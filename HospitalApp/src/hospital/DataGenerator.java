0092366016eba0c71c8e96d40713d2e91294d96e
Luciane Hoshino <2024507@student.cct.ie>
2025-05-03 18:42:39 +0100

    I changed the source for file, instead of use the system, now it will use the same from Applicants_Form.txt, after sort in alphabetical order.

--- HospitalApp/src/hospital/DataGenerator.java
+++ HospitalApp/src/hospital/DataGenerator.java
@@ -3,23 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-package hospital;
+package hospital; // Define the package location
+import java.util.List;
 import java.util.Random;
 
 /**
  *
  * @author lucia
  */
-public class DataGenerator {
-    
-     private static final String[] names = {"Alice Souza", "Bruno De Lucas", "Carlos Almeida", "Diana Smith", "Eva Martins", "Fábio Martins", 
-         "Gabriel Pensador", "Laura Pausine", "Marcelo Telo"};
-    
-   public static Employee generateRandomPerson() {
-        Random rand = new Random();
-        String name = names[rand.nextInt(names.length)];
-        ManagerType manager = ManagerType.values()[rand.nextInt(ManagerType.values().length)];
-        Department dept = Department.values()[rand.nextInt(Department.values().length)];
+ public class DataGenerator {// Define the public class named DataGenerator
+    // This method generates a random Employee using an existing list of Employee objects
+ public static Employee generateRandomPersonFromList(List<Employee> employeeList) 
+   {
+        Random rand = new Random(); // Create a new Random object for generating random indices
+
+        String name = employeeList.get(rand.nextInt(employeeList.size())).getName();// Get a random name by selecting a random Employee and retrieving their name
+        ManagerType manager = ManagerType.values()[rand.nextInt(ManagerType.values().length)]; // Randomly select a ManagerType from the enum values
+        Department dept = Department.values()[rand.nextInt(Department.values().length)];// Randomly select a Department from the enum values
+        // Create and return a new Employee object with the selected name, manager, and department
         return new Employee(name, manager, dept);
-    }
-}
+   }
+ }
